87a6c9acfde7067e23c430c14c468c08ada50655
Adrian Palcic <adrian.palcic@gmail.com>
2026-01-04 14:59:06 +0100

    added messages for the contact form

--- components/ContactHero.tsx
+++ components/ContactHero.tsx
@@ -1,17 +1,20 @@
+import { useTranslations } from "next-intl";
 import React from "react";
 
 const ContactHero = () => {
+  const t = useTranslations("homepage.contact");
+
   return (
     <section className="section-padding w-full">
       <div className="py-10 lg:py-24 px-4 sm:px-12 bg-white mx-auto max-w-3xl relative">
         <div className=" flex flex-col lg:flex-row flex-wrap gap-8 lg:gap-16 items-start justify-end relative">
           <div className="hidden lg:block absolute left-[-120px] bg-(--accent-color) text-white px-6 pt-12 pb-20  flex-[0.4] shadow-2xl">
-            <h2 className="text-3xl font-semibold mb-12">CONTACT US</h2>
+            <h2 className="text-3xl font-semibold mb-12">{t("greenTitle")}</h2>
 
             <ul className="space-y-8">
               <li className="flex items-center gap-4">
                 <span>📍</span>
-                <span>Ulica Petra Kružića 6, Senj</span>
+                <span>{t("Street")}</span>
               </li>
 
               <li className="flex items-center gap-4">
@@ -27,12 +30,12 @@ const ContactHero = () => {
           </div>
           <div className="flex-1 lg:flex-[0.6]">
             <h2 className="text-3xl font-semibold mb-12 uppercase">
-              Get In Touch
+              {t("title")}
             </h2>
             <form className="space-y-6">
               <input
                 type="text"
-                placeholder="Your Name"
+                placeholder={t("placeholder1")}
                 className="w-full bg-gray-100 px-6 py-4 rounded-md"
               />
 
@@ -43,7 +46,7 @@ const ContactHero = () => {
               />
 
               <textarea
-                placeholder="Message"
+                placeholder={t("placeholder2")}
                 rows={6}
                 className="w-full bg-gray-100 px-6 py-4 rounded-md resize-none"
               />
@@ -52,19 +55,19 @@ const ContactHero = () => {
                 type="submit"
                 className="bg-[#A7B58A] text-white px-10 py-3 rounded-full hover:opacity-90 transition"
               >
-                Send
+                {t("button")}
               </button>
             </form>
           </div>
           <div className="block lg:hidden flex-1 bg-(--accent-color) w-full px-2 py-12 sm:p-12 text-white">
             <h2 className="text-3xl text-center sm:text-left font-semibold mb-12">
-              CONTACT US
+              {t("greenTitle")}
             </h2>
 
             <ul className="space-y-8">
               <li className="flex items-center gap-4">
                 <span>📍</span>
-                <span>Ulica Petra Kružića 6, Senj</span>
+                <span>{t("Street")}</span>
               </li>
 
               <li className="flex items-center gap-4">
