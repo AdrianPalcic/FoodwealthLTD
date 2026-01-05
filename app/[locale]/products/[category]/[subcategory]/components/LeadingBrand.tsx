0f8a25dd73c8f74776c6fbd2c8b202b32cd5b821
Adrian Palcic <adrian.palcic@gmail.com>
2026-01-05 13:48:20 +0100

    some translations

--- app/[locale]/products/[category]/[subcategory]/components/LeadingBrand.tsx
+++ app/[locale]/products/[category]/[subcategory]/components/LeadingBrand.tsx
@@ -1,10 +1,13 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import React from "react";
 
 const LeadingBrand = () => {
+  const t = useTranslations("subcategorypage.whyus");
+
   return (
     <section className="section-padding">
-      <h1 className="title text-center mb-4">Explore our leading brand</h1>
+      <h1 className="title text-center mb-4">{t("leadingbrand")}</h1>
       <div className="w-fit mx-auto">
         <Link href="https://www.capeherb.co.za/">
           <img
--- app/[locale]/products/[category]/[subcategory]/components/WhyUs.tsx
+++ app/[locale]/products/[category]/[subcategory]/components/WhyUs.tsx
@@ -1,14 +1,14 @@
+import { useTranslations } from "next-intl";
 import React from "react";
 
 const WhyUs = () => {
+  const t = useTranslations("subcategorypage.whyus");
+
   return (
     <section className="section-padding">
       <div className="flex flex-col items-center justify-center w-full">
-        <h1 className="title text-center">Why Choose Us?</h1>
-        <h2 className="subtitle text-center max-w-5xl">
-          Experience the difference that quality, sustainability, and passion
-          make in every product.
-        </h2>
+        <h1 className="title text-center">{t("title")}</h1>
+        <h2 className="subtitle text-center max-w-5xl">{t("subtitle")}</h2>
       </div>
       <div className="grid grid-rows-2 grid-cols-1 md:grid-cols-2 mt-10 gap-6">
         <div className="bg-(--secondary-color) row-span-2 min-h-[300px] sm:min-h-[600px] relative flex flex-col justify-center rounded-xl py-6 px-4 sm:px-10">
@@ -18,16 +18,12 @@ const WhyUs = () => {
             loading="lazy"
             className="hidden sm:block w-full h-full object-cover inset-0 rounded-xl absolute"
           />
-          <div className="absolute inset-0 w-full h-full z-2 bg-black/40 rounded-xl hidden sm:block"></div>
+          <div className="absolute inset-0 w-full h-full z-2 bg-black/50 rounded-xl hidden sm:block"></div>
           <div className="z-10">
             <h1 className="uppercase text-3xl sm:text-4xl lg:text-5xl font-bold text-white mb-2">
-              Premium Quality Guaranteed
+              {t("title2")}
             </h1>
-            <p className="text-white">
-              Every spice undergoes rigorous testing and quality control.
-              Handpicked at peak freshness, sourced from the world's finest
-              spice gardens, and delivered directly to your kitchen.
-            </p>
+            <p className="text-white">{t("subtitle2")}</p>
           </div>
         </div>
         <div className="row-span-1 rounded-xl bg-white py-6 px-4 sm:px-6">
@@ -37,12 +33,9 @@ const WhyUs = () => {
             </div>
             <div className="flex flex-col gap-4">
               <h3 className="text-2xl sm:text-4xl font-bold uppercase ">
-                Freshness Guaranteed
+                {t("title3")}
               </h3>
-              <p className="subtitle">
-                Small-batch processing ensures maximum flavor and aroma in every
-                package.
-              </p>
+              <p className="subtitle">{t("subtitle3")}</p>
             </div>
           </div>
         </div>
@@ -53,12 +46,9 @@ const WhyUs = () => {
             </div>
             <div className="flex flex-col gap-4">
               <h3 className="text-2xl sm:text-4xl font-bold uppercase ">
-                Sustainably Sourced
+                {t("title4")}
               </h3>
-              <p className="subtitle">
-                Small-batch processing ensures maximum flavor and aroma in every
-                package.
-              </p>
+              <p className="subtitle">{t("subtitle4")}</p>
             </div>
           </div>
         </div>
